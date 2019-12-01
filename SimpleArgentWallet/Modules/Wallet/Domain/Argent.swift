14c4d05861836793fd0ec0e73f3f44fd512ea950
Jakub Tomanik <j.tomanik@gmail.com>
2019-12-01 17:08:05 +0100

    Added Argent Domain models

---
+++ SimpleArgentWallet/Modules/Wallet/Domain/Argent.swift
@@ -0,0 +1,28 @@
+//
+//  Argent.swift
+//  SimpleArgentWallet
+//
+//  Created by Jakub Tomanik on 01/12/2019.
+//  Copyright © 2019 Jakub Tomanik. All rights reserved.
+//
+
+import Foundation
+import BigInt
+
+struct Argent {}
+
+extension Argent {
+
+    struct TransferManager {
+        static let contractAddress = Ethereum.Address(hexString: "0xcdAd167a8A9EAd2DECEdA7c8cC908108b0Cc06D1")!
+
+        struct TransferTokenInputs {
+            static let name = "transferToken"
+            
+            let wallet: Ethereum.Address
+            let token: Ethereum.Address
+            let to: Ethereum.Address
+            let amount: BigUInt
+        }
+    }
+}
