94a66e72fc177bf9848ec4d42d9de9aeef4c1810
Jakub Tomanik <j.tomanik@gmail.com>
2019-12-01 17:09:13 +0100

    Added basic service to execute Ethereum calls

---
+++ SimpleArgentWallet/Modules/Wallet/Services/EthereumRequester.swift
@@ -0,0 +1,38 @@
+//
+//  EthereumRequester.swift
+//  SimpleArgentWallet
+//
+//  Created by Jakub Tomanik on 01/12/2019.
+//  Copyright © 2019 Jakub Tomanik. All rights reserved.
+//
+
+import RxSwift
+import web3
+import BigInt
+
+class EthereumRequester {
+
+    let defaultGatewayURL = URL(string: "https://ropsten.infura.io/v3/735489d9f846491faae7a31e1862d24b")!
+
+    let gatewayURL: URL
+    let client: EthereumClient
+
+    init(gateway: URL? = nil) {
+        self.gatewayURL = gateway ?? defaultGatewayURL
+        self.client = EthereumClient(url: gatewayURL)
+    }
+}
+
+extension Ethereum.Address {
+
+    func toWeb3() -> EthereumAddress {
+        return EthereumAddress(self.hexString)
+    }
+}
+
+extension EthereumAddress {
+
+    func toDomain() -> Ethereum.Address {
+        return Ethereum.Address(hexString: self.value)!
+    }
+}
